7e9d62f2f669db5f14ba152c099e7d5aed73d5f8
farshad <farshad.badiee@gmail.com>
2024-09-23 10:07:19 +0330

    adding new service

--- models.py
+++ models.py
@@ -5,29 +5,29 @@ from database import Base
 import datetime
 
 
-class PronunciationResponse(Base):
-    __tablename__ = "pronunciation_responses"
-    id = Column(Integer, primary_key=True, index=True)
-    overall_score = Column(Float, nullable=False)
-    expected_text = Column(String, nullable=False)
-    mock_ielts_prediction = Column(Float, nullable=True)
-    mock_cefr_prediction = Column(String, nullable=True)
-    mock_pte_prediction = Column(Integer, nullable=True)
-    lowest_scoring_phonemes = Column(JSON, nullable=True)
-    warnings = Column(JSON, nullable=True)
-    response_id = Column(Integer, ForeignKey("main_responses.id"), nullable=False)
-    words = relationship("WordDetails", back_populates="pronunciation_response")
-
-
-class WordDetails(Base):
-    __tablename__ = "word_details"
-    id = Column(Integer, primary_key=True, index=True)
-    word_text = Column(String, nullable=False)
-    word_score = Column(Float, nullable=False)
-    phonemes = Column(JSON, nullable=False)
-    pronunciation_response_id = Column(Integer, ForeignKey("pronunciation_responses.id"), nullable=False)
-    pronunciation_response = relationship("PronunciationResponse", back_populates="words")
-
+# class PronunciationResponse(Base):
+#     __tablename__ = "pronunciation_responses"
+#     id = Column(Integer, primary_key=True, index=True)
+#     overall_score = Column(Float, nullable=False)
+#     expected_text = Column(String, nullable=False)
+#     mock_ielts_prediction = Column(Float, nullable=True)
+#     mock_cefr_prediction = Column(String, nullable=True)
+#     mock_pte_prediction = Column(Integer, nullable=True)
+#     lowest_scoring_phonemes = Column(JSON, nullable=True)
+#     warnings = Column(JSON, nullable=True)
+#     response_id = Column(Integer, ForeignKey("main_responses.id"), nullable=False)
+#     words = relationship("WordDetails", back_populates="pronunciation_response")
+#
+#
+# class WordDetails(Base):
+#     __tablename__ = "word_details"
+#     id = Column(Integer, primary_key=True, index=True)
+#     word_text = Column(String, nullable=False)
+#     word_score = Column(Float, nullable=False)
+#     phonemes = Column(JSON, nullable=False)
+#     pronunciation_response_id = Column(Integer, ForeignKey("pronunciation_responses.id"), nullable=False)
+#     pronunciation_response = relationship("PronunciationResponse", back_populates="words")
+#
 
 class ServiceResult(Base):
     __tablename__ = "service_results"
